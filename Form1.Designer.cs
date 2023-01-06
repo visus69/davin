c6f3e4a20ba66ecbe56ac09ad88f266e27777f55
MUHAMMAD JUNIOR <mjunior@students.amikom.ac.id>
2023-01-06 15:40:30 +0700

    sss

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,24 +36,25 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(105, 262);
+            this.textBox1.Location = new System.Drawing.Point(106, 361);
             this.textBox1.Name = "textBox1";
             this.textBox1.PasswordChar = '*';
             this.textBox1.Size = new System.Drawing.Size(300, 22);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "sss";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 60);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(105, 209);
+            this.textBox2.Location = new System.Drawing.Point(87, 51);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(300, 22);
+            this.textBox2.Size = new System.Drawing.Size(388, 22);
             this.textBox2.TabIndex = 2;
             // 
             // Form1
